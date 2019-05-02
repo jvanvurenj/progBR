e5883c40291f7100bb7c6ed601fa92c96049f285
Ryan <oglesby_ryan@yahoo.com>
2019-05-02 15:59:52 -0700

    Health working + HP Bars working on multiplayer

--- ProgressiveBR/Assets/Scripts/HealthManager.cs
+++ ProgressiveBR/Assets/Scripts/HealthManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class HealthManager : NetworkBehaviour
 {
@@ -9,12 +10,38 @@ public class HealthManager : NetworkBehaviour
     // Starting health can be set to whatever.
     [SerializeField]
     private float playerHealth = 100;
+    private float startingHP;
     public bool isAlive = true;
+    public Image hpBar;
+
+    private void Start()
+    {
+        startingHP = playerHealth;
+    }
+
+    [Command]
+    public void CmdHpBar(float amt)
+    {
+        RpcHpBar(amt);
+    }
+
+    [ClientRpc]
+    public void RpcHpBar(float amt)
+    {
+        
+         hpBar.fillAmount = amt;
+        
+    }
 
     public void TakeDamage(float amt)
     {
-        
+
+       
         playerHealth -= amt;
+        hpBar.fillAmount = playerHealth / startingHP;
+        CmdHpBar(playerHealth/startingHP);
+        
+
         if(playerHealth <= 0)
         {
             // Perform other logics like giving points to whoever killed this person
