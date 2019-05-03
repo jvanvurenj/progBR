5e8a58b23de0711ee5df8158c63dc90e5302fe82
Ryan <oglesby_ryan@yahoo.com>
2019-05-02 18:32:30 -0700

    Player HP Bar faces user, need to make it appear different for the rest of the server

---
+++ ProgressiveBR/Assets/Scripts/FaceGuiToPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FaceGuiToPlayer : NetworkBehaviour
+{
+
+    private Camera thisCamera;
+    public Canvas thisCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = GetComponent<PlayerMovement>().MyCamera;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) { return; }
+        Vector3 targetPostition = new Vector3(thisCamera.transform.position.x, thisCanvas.transform.position.y, thisCamera.transform.position.z);
+        thisCanvas.transform.LookAt(targetPostition);
+      
+    }
+}
