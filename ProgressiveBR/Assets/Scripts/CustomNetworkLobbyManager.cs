b4778e65a6dd66f1fea53e42aef01644c9124e52
Halle King <halle.king@yahoo.com>
2019-05-28 21:04:04 -0700

    Custom lobby, new fonts for scenes, new urban scene

---
+++ ProgressiveBR/Assets/Scripts/CustomNetworkLobbyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class CustomNetworkLobbyManager : NetworkLobbyManager
+{
+    // Generate map randomly when all players are ready
+    public override void OnLobbyServerPlayersReady()
+    {
+        string[] scenes = new string[3] { "ProgressiveBR", "SnowBR", "DesertBR" };
+        int random = Random.Range(0, 3);
+        playScene = scenes[random];
+        ServerChangeScene(playScene);
+    }
+
+}
+
+
